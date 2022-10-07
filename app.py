2d114efa9b0271707ca9fb055a86f0418e289add
Binyameen <bmghafoor@gmail.com>
2022-10-06 22:42:16 -0400

    session

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, flash
+from flask import Flask, render_template, redirect, request, flash, session
 from surveys import satisfaction_survey
 
 
@@ -8,6 +8,11 @@ app.secret_key = 'password123'
 
 responses = []
 
+@app.route('/start', methods = ['POST'])
+def start():
+    session['responses'] = []
+    return redirect('/question/0')
+
 @app.route('/')
 def homepage():
     return render_template('base.html', title = satisfaction_survey.title, instructions = satisfaction_survey.instructions)
@@ -15,20 +20,24 @@ def homepage():
 @app.route('/question/<int:num>')
 def question(num):
 
-    if len(responses) == len(satisfaction_survey.questions):
+    if len(session['responses']) == len(satisfaction_survey.questions):
         return "Thank You for Completing the Survey, You may exit out this window"
 
-    elif num == len(responses):
+    elif num == len(session['responses']):
         return render_template('index.html', title = satisfaction_survey.title, q = satisfaction_survey.questions[num])
 
-    elif num != len(responses):
+    elif num != len(session['responses']):
         flash('Stop messing with the url')
-        return render_template('index.html', title = satisfaction_survey.title, q = satisfaction_survey.questions[len(responses)])
+        return render_template('index.html', title = satisfaction_survey.title, q = satisfaction_survey.questions[len(session['responses'])])
     
 
 @app.route('/answers', methods = ['POST'])
 def answer():
     ans = request.form['ans']
+    responses = session['responses']
     responses.append(ans)
-    return redirect(f'/question/{len(responses)}')
+    session['responses'] = responses
+    id = len(session['responses'])
+    return redirect(f'/question/{id}')
+
 
